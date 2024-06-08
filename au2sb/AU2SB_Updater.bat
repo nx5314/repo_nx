ce752253ce65ddc28a806f6162de2eeace424a68
nx5314 <mikleo@duck.com>
2024-06-08 04:33:42 -0700

    Update AU2SB_Updater.bat

--- au2sb/AU2SB_Updater.bat
+++ au2sb/AU2SB_Updater.bat
@@ -35,27 +35,35 @@ for /f "delims=" %%i in ('curl -s https://raw.githubusercontent.com/nx5314/repo_
 for /f "delims=" %%i in ('curl -s https://raw.githubusercontent.com/nx5314/repo_nx/main/au2sb/resourcepacks.txt') do set "resourcepacks_url=%%i"
 for /f "delims=" %%i in ('curl -s https://raw.githubusercontent.com/nx5314/repo_nx/main/au2sb/extras.txt') do set "extras_url=%%i"
 
-echo Downloading modpack...
+echo Starting modpack update...
 if exist "%temp%\au2sb" rmdir "%temp%\au2sb" /s /q 2>&1 >nul
 mkdir "%temp%\au2sb" 2>&1 >nul
+echo Downloading mods...
 curl -L "%mods_url%" --output "%temp%\au2sb_mods.zip"
+echo Downloading config...
 curl -L "%config_url%" --output "%temp%\au2sb_config.zip"
+echo Downloading resourcepacks...
 curl -L "%resourcepacks_url%" --output "%temp%\au2sb_resourcepacks.zip"
+echo Downloading extras...
 curl -L "%extras_url%" --output "%temp%\au2sb_extras.zip"
+echo Extracting mods...
 tar -xf "%temp%\au2sb_mods.zip" -C "%temp%\au2sb" 2>&1 >nul
+echo Extracting config...
 tar -xf "%temp%\au2sb_config.zip" -C "%temp%\au2sb" 2>&1 >nul
+echo Extracting resourcepacks...
 tar -xf "%temp%\au2sb_resourcepacks.zip" -C "%temp%\au2sb" 2>&1 >nul
+echo Extracting extras...
 tar -xf "%temp%\au2sb_extras.zip" -C "%temp%\au2sb" 2>&1 >nul
-del "%temp%\au2sb_mods.zip" /q 2>&1 >nul
-del "%temp%\au2sb_config.zip" /q 2>&1 >nul
-del "%temp%\au2sb_resourcepacks.zip" /q 2>&1 >nul
-del "%temp%\au2sb_extras.zip" /q 2>&1 >nul
+echo Moving files to %minecraftfolder%...
 robocopy "%temp%\au2sb\mods" "%minecraftfolder%\mods" /s /purge /r:100 /move /log:"%temp%\au2sb_mods.log" 2>&1 >nul
 robocopy "%temp%\au2sb\config" "%minecraftfolder%\config" /s /r:100 /move /log:"%temp%\au2sb_config.log" 2>&1 >nul
 robocopy "%temp%\au2sb\resourcepacks" "%minecraftfolder%\resourcepacks" /s /r:100 /move /log:"%temp%\au2sb_resourcepacks.log" 2>&1 >nul
 robocopy "%temp%\au2sb" "%minecraftfolder%" /s /r:100 /move /log:"%temp%\au2sb_extras.log" 2>&1 >nul
-
-rmdir "%temp%\au2sb" /s /q 2>&1 >nul
+echo Cleaning up leftovers...
+del "%temp%\au2sb_mods.zip" /q 2>&1 >nul
+del "%temp%\au2sb_config.zip" /q 2>&1 >nul
+del "%temp%\au2sb_resourcepacks.zip" /q 2>&1 >nul
+del "%temp%\au2sb_extras.zip" /q 2>&1 >nul
 
 :: Check fabric installation
 set "fabric_exists=false"
@@ -66,7 +74,7 @@ if "%fabric_exists%"=="false" (
 	echo.
 	echo Fabric appears to not be installed, downloading now
     curl -L https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.0.1/fabric-installer-1.0.1.jar --output "%temp%\fabric-installer.jar"
-    java -jar %temp%\fabric-installer.jar client -dir %appdata%\.minecraft
+    java -jar %temp%\fabric-installer.jar client -mcversion 1.20.1 -dir %appdata%\.minecraft
 	echo.
 	echo Fabric installed
 )
