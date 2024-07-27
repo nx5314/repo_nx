18c11a2091a9b9f495f289d2be743bacb30b3f9c
nx5314 <mikleo@duck.com>
2024-07-27 00:53:51 -0700

    Update AU2SB_Updater.bat

--- au2sb/AU2SB_Updater.bat
+++ au2sb/AU2SB_Updater.bat
@@ -173,9 +173,11 @@ echo         3. Uninstall
 echo         4. Move install location
 echo         5. Distant Horizons Data
 echo         6. About
+echo         9. Exit
 echo.
     title %title_prompt%
     set /p "startup_selection=Select: "
+    if "!startup_selection!"=="9" exit /b
     if not "!startup_selection!"=="1" if not "!startup_selection!"=="2" if not "!startup_selection!"=="3" if not "!startup_selection!"=="4" if not "!startup_selection!"=="5" if not "!startup_selection!"=="6" (
         echo Invalid selection, please enter a number between 1 and 6.
         echo.
